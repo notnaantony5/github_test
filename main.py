d2d636f5a3dc5e6fa705fd27b58060c842efeee3
text <test@test.com>
2024-08-01 16:38:26 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from typing import Sized
-
-infos = [("Андрей", 33, "Python"), ("Антон", 26, "JS")]
-name, _, course = infos[0]
-print(name, course)
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(my_list):
+    print(my_list[index])
+    index = index + 1
